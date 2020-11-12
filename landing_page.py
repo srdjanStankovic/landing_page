43458c2bdbb978d2d9ccac82803ead2e42c7c1a9
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-11-12 18:17:22 +0100

    rename base to database; verify email entrance;

--- landing_page.py
+++ landing_page.py
@@ -9,10 +9,10 @@ from email_validator import validate_email, EmailNotValidError
 logging.basicConfig(level=logging.DEBUG)
 application = Flask(__name__)
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_base.sqlite3'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_database.sqlite3'
 
 db = SQLAlchemy(application)
-class users_base(db.Model):
+class users_database(db.Model):
    id = db.Column('users_id', db.Integer, primary_key = True)
    email = db.Column(db.String(254))
    datetime = db.Column(db.Integer)
@@ -67,19 +67,20 @@ def landing_page():
     if request.method == "POST":
         logging.debug("It's POST")
     elif request.method == "GET":
-        email = request.args.get('input_field')
+        email = request.args.get('email')
         if validate_inserted_email(email):
             logging.debug("It's GET\n Email is: " + email)
 
             utc = datetime.utcnow()
-            entrance = users_base(email, utc)
+            entrance = users_database(email, utc)
             db.session.add(entrance)
             db.session.commit()
         else:
-            #TODO: raise msg to client
+            #TODO: raise oninvalid
             logging.error("Not valid email!")
 
     return render_template("index.html", parameters = parameters)
+
 
 if __name__ == '__main__':
     logging.info("Web server started!")
