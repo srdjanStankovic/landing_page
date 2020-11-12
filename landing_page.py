99750305665d54dbab8a14ce6944ef1225a315f0
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-11-12 18:24:12 +0100

    hierarchy improvements

--- landing_page.py
+++ landing_page.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import logging
-import yaml
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from email_validator import validate_email, EmailNotValidError
+from utils import read_configs, validate_inserted_email
 
 logging.basicConfig(level=logging.DEBUG)
 application = Flask(__name__)
@@ -20,47 +19,6 @@ class users_database(db.Model):
    def __init__(self, email, datetime):
       self.email = email
       self.datetime = datetime
-
-def read_configs():
-    parameters = [""] * 5
-
-    with open(r'config.yaml') as file:
-        # The FullLoader parameter handles the conversion from YAML
-        # scalar values to Python the dictionary format
-        fruits_list = yaml.load(file, Loader=yaml.FullLoader)
-
-        for key, value in fruits_list.items():
-            logging.info(key + " : " + str(value))
-
-            if key == "host":
-                parameters[0] = value
-            if key == "port":
-                parameters[1] = value
-            elif key == "facebook":
-                parameters[2] = value
-            elif key == "instagram":
-                parameters[3] = value
-            elif key == "youtube":
-                parameters[4] = value
-            else:
-                logging.error("Unsupported key: " + key)
-        logging.info(parameters)
-        return parameters
-
-def validate_inserted_email(email):
-    logging.debug(email)
-    if email == None:
-        return False
-
-    try:
-      valid = validate_email(email)
-      email = valid.email
-    except EmailNotValidError as e:
-      # email is not valid, exception message is human-readable
-      logging.error(str(e))
-      return False
-
-    return True
 
 @application.route("/",methods = ['POST', 'GET'])
 def landing_page():
