cf7fe6d34cd932815a34abbf6324bf0b059ee484
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-12-16 23:10:16 +0100

    init before main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import logging
 from sources import app
-from sources.utils import read_configs, initialization
-from sources.models import db, users_database, init_application
+from sources.utils import read_configs
+from sources.models import db
+from sources.models import init_application
 
 logging.basicConfig(level=logging.DEBUG)
 # TODO: Heroku specific issues(delete data base after shootdown), Database are problems!
+init_application(app)
 if __name__ == "__main__":
-    #initialization()
-    init_application(app)
     parameters = read_configs()
     app.run(parameters[0], parameters[1])
